6c218922df77c73c6896f85b9ceb25ea45fa5b26
Alekasandr26 <a.brendin@mail.ru>
2021-10-04 11:37:30 +0400

    Create Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import React from 'react';
 import './App.css';
-import Quiz from './components/Quiz/Quiz';
 import Layout from './hoc/Layout/Layout';
 
 const App = () => {
   return (
     <Layout>
-       <Quiz/>
+
     </Layout>
   );
 };
