8f66207a555d97b491609e32b7d542d3fa6221a4
Alekasandr26 <a.brendin@mail.ru>
2021-10-04 11:35:44 +0400

    Create Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
 
-function App() {
+import React from 'react';
+import './App.css';
+import Quiz from './components/Quiz/Quiz';
+import Layout from './hoc/Layout/Layout';
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Layout>
+       <Quiz/>
+    </Layout>
   );
-}
+};
 
 export default App;
